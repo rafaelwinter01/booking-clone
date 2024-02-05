b2152c964523501c9763accb3d8fe5a774bf6e28
rafaelwinter01 <rafaelwinter01@gmail.com>
2024-02-05 13:01:03 -0800

    New Version

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -12,7 +12,8 @@ async function SearchPage({ searchParams }: SearchPageProps) {
   if (!searchParams.url) return notFound();
 
   const results = await fetchResults(searchParams);
-  if (!results) return <div>No results were found...</div>;
+  if (!results || !results.content.listings)
+    return <div>No results were found...</div>;
   console.log("Total results found: ", results.content.listings.length);
 
   return (
